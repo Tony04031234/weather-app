e72f2cec8d837c7704a8021dc2149dad69a1b6df
TonyynoT <55374414+Tony04031234@users.noreply.github.com>
2021-04-18 14:02:49 +0930

    added WeatherDetails.js

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { Platform, Text, View, StyleSheet, ActivityIndicator} from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator} from 'react-native';
 import * as Location from 'expo-location'
 
 import WeatherInfo from './components/WeatherInfo'
 import UnitsPicker from './components/UnitsPicker'
-import { colors } from './utils/index'
 import ReloadIcon from './components/ReloadIcon'
+import WeatherDetails from './components/WeatherDetails'
+import { colors } from './utils/index'
 
 
 const WEATHER_API_KEY = 'a65200c5775b2876b313e125ed8ffad6'
@@ -53,11 +54,14 @@ export default function App() {
 		return(
 			<View style={styles.container}>
 				<StatusBar style="auto" />
+				
 				<View style={styles.main} >
-					<WeatherInfo currentWeather={currentWeather} />
-					<ReloadIcon load={load} />
 					<UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem} />
+					<ReloadIcon load={load} />
+					<WeatherInfo currentWeather={currentWeather} />
 				</View>
+
+				<WeatherDetails currentWeather={currentWeather} unitsSystem={unitsSystem} />
 			</View>
 		)
 	}
